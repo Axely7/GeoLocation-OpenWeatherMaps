e7a5abfbe281cbe5631d23490f86a09ca1522344
Axely7 <axely7@hotmail.com>
2022-10-24 09:38:05 -0500

    creating app

---
+++ index.js
@@ -0,0 +1,40 @@
+import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
+import { Busquedas } from "./models/busquedas.js";
+
+const main = async () => {
+  let opt;
+  const busquedas = new Busquedas();
+
+  do {
+    // Imprimir el menu
+    opt = await inquirerMenu();
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+        // Buscar los lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        // Mostrar Resultados:
+
+        console.log("\nInformación de la ciudad\n".green);
+        console.log("Ciudad: ");
+        console.log("Lat: ");
+        console.log("Lng: ");
+        console.log("Temperatura: ");
+        console.log("Mínima:");
+        console.log("Máxima:");
+        break;
+
+      case 2:
+        console.log("Opcion 2");
+    }
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
+};
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,14 @@
+class Busquedas {
+  historial = ["Tegucigalpa", "Madrid", "San José"];
+
+  constructor() {}
+
+  async ciudad(lugar = "") {
+    // peticion http
+    console.log(lugar);
+
+    return [];
+  }
+}
+
+export { Busquedas };
